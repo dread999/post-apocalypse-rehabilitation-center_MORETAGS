7f58582e780177890d71486c4c055d1276cd64fd
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2026-01-12 19:39:48 +0000

    Looking at weird saving location issue.

--- src/Stage.tsx
+++ src/Stage.tsx
@@ -228,6 +228,7 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
         const saves: (SaveType | undefined)[] = Array(this.SAVE_SLOTS).fill(undefined);
         for (let slot = 0; slot < this.SAVE_SLOTS; slot++) {
             promises.push(async () => {
+                console.log(`Fetching save data for slot ${slot} from storage API.`);
                 const response = await this.storage.get(`saveData_${slot}`).forUser(this.userId);
                 console.log('Fetched save slot data from storage API:', response);
 
@@ -243,6 +244,7 @@ export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateT
             });
         }
         await Promise.all(promises);
+        console.log('Finished loading save data from storage API.');
         
         // If any saves were loaded, use them:
         if (saves.some(save => save !== undefined)) {
